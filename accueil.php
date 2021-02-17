015670fb9aa2c445f9ef29c3c5999b66df603075
emmaveauvy <78910688+emmaveauvy@users.noreply.github.com>
2021-02-17 08:34:45 +0100

    Create accueil.php

---
+++ accueil.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link href="style_loader.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <div>Voici la page d'accueil</div>
+
+</body>
+
+</html>
